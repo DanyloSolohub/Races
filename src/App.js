77ffb581833ab5f4c06e0f5a469d631b18712bc7
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-02-12 18:27:39 +0200

    race done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
@@ -62,16 +62,69 @@ export default function App() {
         moveRight: false,
         rotate: -90
     }
-    const first_turn = {
-
+    const first_turn_out = {
+        width: 10,
+        height: 630,
+        x: 170,
+        y: 170,
     }
+    const first_turn_in = {
+        width: 10,
+        height: 630,
+        x: 180,
+        y: 170,
+    }
+    const second_turn_out = {
+        width: 660,
+        height: 10,
+        x: 170,
+        y: 180,
+    }
+    const second_turn_in = {
+        width: 660,
+        height: 10,
+        x: 170,
+        y: 190,
+    }
+    const third_turn_out = {
+        width: 10,
+        height: 560,
+        x: 830,
+        y: 170,
+    }
+    const third_turn_in = {
+        width: 10,
+        height: 560,
+        x: 820,
+        y: 170,
+    }
+    const fourth_turn_out = {
+        width: 470,
+        height: 10,
+        x: 270,
+        y: 630,
+    }
+    const fourth_turn_in = {
+        width: 470,
+        height: 10,
+        x: 270,
+        y: 640,
+    }
+    const finish = {
+        width: 40,
+        height: 40,
+        x: 500,
+        y: 400,
+    }
+    const [countYellow, setCountB] = useState(0)
+    const [countBlue, setCountY] = useState(0)
     useEffect(() => {
         const car_1 = document.getElementById('driftingCar_1')
-         const car_2 = document.getElementById('driftingCar_2')
+        const car_2 = document.getElementById('driftingCar_2')
         setInterval(() => {
             if (driftingCar_1.moveFront === true) {
-                driftingCar_1.y = driftingCar_1.y - Math.sin(Math.PI / 180 * Math.abs(driftingCar_1.rotate))* 2
-                driftingCar_1.x = driftingCar_1.x + Math.cos(Math.PI / 180 * Math.abs(driftingCar_1.rotate))* 2
+                driftingCar_1.y = driftingCar_1.y - Math.sin(Math.PI / 180 * Math.abs(driftingCar_1.rotate)) * 2
+                driftingCar_1.x = driftingCar_1.x + Math.cos(Math.PI / 180 * Math.abs(driftingCar_1.rotate)) * 2
                 car_1.style.top = driftingCar_1.y + 'px'
                 car_1.style.left = driftingCar_1.x + 'px'
             }
@@ -112,6 +165,39 @@ export default function App() {
             } else if (collides(driftingCar_1, rightWall) === true) {
                 driftingCar_1.x = driftingCar_1.x - 3
                 car_1.style.left = driftingCar_1.x + 'px'
+            } else if (collides(driftingCar_1, first_turn_out) === true) {
+                driftingCar_1.x = driftingCar_1.x - 3
+                car_1.style.left = driftingCar_1.x + 'px'
+            } else if (collides(driftingCar_1, first_turn_in) === true) {
+                driftingCar_1.x = driftingCar_1.x + 3
+                car_1.style.left = driftingCar_1.x + 'px'
+            } else if (collides(driftingCar_1, second_turn_out) === true) {
+                driftingCar_1.y = driftingCar_1.y - 3
+                car_1.style.top = driftingCar_1.y + 'px'
+            } else if (collides(driftingCar_1, second_turn_in) === true) {
+                driftingCar_1.y = driftingCar_1.y + 3
+                car_1.style.top = driftingCar_1.y + 'px'
+            } else if (collides(driftingCar_1, third_turn_out) === true) {
+                driftingCar_1.x = driftingCar_1.x + 3
+                car_1.style.left = driftingCar_1.x + 'px'
+            } else if (collides(driftingCar_1, third_turn_in) === true) {
+                driftingCar_1.x = driftingCar_1.x - 3
+                car_1.style.left = driftingCar_1.x + 'px'
+            } else if (collides(driftingCar_1, fourth_turn_out) === true) {
+                driftingCar_1.y = driftingCar_1.y - 3
+                car_1.style.top = driftingCar_1.y + 'px'
+            } else if (collides(driftingCar_1, fourth_turn_in) === true) {
+                driftingCar_1.y = driftingCar_1.y + 3
+                car_1.style.top = driftingCar_1.y + 'px'
+            } else if (collides(driftingCar_1, finish) === true) {
+                setCountY((prev) => prev + 1)
+                    driftingCar_1.x = 20
+                driftingCar_1.y = 750
+                driftingCar_1.rotate = -90
+                car_1.style.top = driftingCar_1.y + 'px'
+                car_1.style.left = driftingCar_1.x + 'px'
+                car_1.style.transform = `rotate(${driftingCar_1.rotate}deg)`
+
             }
 
             if (driftingCar_1.rotate < -359) {
@@ -119,9 +205,10 @@ export default function App() {
             } else if (driftingCar_1.rotate > 0) {
                 driftingCar_1.rotate = -360
             }
+            // second player
             if (driftingCar_2.moveFront === true) {
-                driftingCar_2.y = driftingCar_2.y - Math.sin(Math.PI / 180 * Math.abs(driftingCar_2.rotate))* 2
-                driftingCar_2.x = driftingCar_2.x + Math.cos(Math.PI / 180 * Math.abs(driftingCar_2.rotate))* 2
+                driftingCar_2.y = driftingCar_2.y - Math.sin(Math.PI / 180 * Math.abs(driftingCar_2.rotate)) * 2
+                driftingCar_2.x = driftingCar_2.x + Math.cos(Math.PI / 180 * Math.abs(driftingCar_2.rotate)) * 2
                 car_2.style.top = driftingCar_2.y + 'px'
                 car_2.style.left = driftingCar_2.x + 'px'
             }
@@ -162,6 +249,39 @@ export default function App() {
             } else if (collides(driftingCar_2, rightWall) === true) {
                 driftingCar_2.x = driftingCar_2.x - 3
                 car_2.style.left = driftingCar_2.x + 'px'
+            }else if (collides(driftingCar_2, first_turn_out) === true) {
+                driftingCar_2.x = driftingCar_2.x - 3
+                car_2.style.left = driftingCar_2.x + 'px'
+            } else if (collides(driftingCar_2, first_turn_in) === true) {
+                driftingCar_2.x = driftingCar_2.x + 3
+                car_2.style.left = driftingCar_2.x + 'px'
+            } else if (collides(driftingCar_2, second_turn_out) === true) {
+                driftingCar_2.y = driftingCar_2.y - 3
+                car_2.style.top = driftingCar_2.y + 'px'
+            } else if (collides(driftingCar_2, second_turn_in) === true) {
+                driftingCar_2.y = driftingCar_2.y + 3
+                car_2.style.top = driftingCar_2.y + 'px'
+            } else if (collides(driftingCar_2, third_turn_out) === true) {
+                driftingCar_2.x = driftingCar_2.x + 3
+                car_2.style.left = driftingCar_2.x + 'px'
+            } else if (collides(driftingCar_2, third_turn_in) === true) {
+                driftingCar_2.x = driftingCar_2.x - 3
+                car_2.style.left = driftingCar_2.x + 'px'
+            } else if (collides(driftingCar_2, fourth_turn_out) === true) {
+                driftingCar_2.y = driftingCar_2.y - 3
+                car_2.style.top = driftingCar_2.y + 'px'
+            } else if (collides(driftingCar_2, fourth_turn_in) === true) {
+                driftingCar_2.y = driftingCar_2.y + 3
+                car_2.style.top = driftingCar_2.y + 'px'
+            } else if (collides(driftingCar_2, finish) === true) {
+                setCountB((prev) => prev + 1)
+                driftingCar_2.x = 90
+                driftingCar_2.y = 750
+                driftingCar_2.rotate = -90
+                car_2.style.top = driftingCar_2.y + 'px'
+                car_2.style.left = driftingCar_2.x + 'px'
+                car_2.style.transform = `rotate(${driftingCar_2.rotate}deg)`
+
             }
 
             if (driftingCar_2.rotate < -359) {
@@ -192,8 +312,8 @@ export default function App() {
             driftingCar_1.moveLeft = true
         }
     }
-    const startMoving_2 = function (e){
-         // second player start
+    const startMoving_2 = function (e) {
+        // second player start
         if (e.which === 87) {
             driftingCar_2.moveFront = true
             driftingCar_2.moveBack = false
@@ -211,7 +331,7 @@ export default function App() {
     }
     const stopMoving_1 = function (e) {
         // first player stop
-        if (e.which === 38 ) {
+        if (e.which === 38) {
             driftingCar_1.moveFront = false
         } else if (e.which === 40) {
             driftingCar_1.moveBack = false
@@ -225,14 +345,14 @@ export default function App() {
     }
     const stopMoving_2 = function (e) {
         // second player stop
-        if ( e.which === 87 ) {
+        if (e.which === 87) {
             driftingCar_2.moveFront = false
-        } else if ( e.which === 83) {
+        } else if (e.which === 83) {
             driftingCar_2.moveBack = false
         }
-        if ( e.which === 68) {
+        if (e.which === 68) {
             driftingCar_2.moveRight = false
-        } else if ( e.which === 65) {
+        } else if (e.which === 65) {
             driftingCar_2.moveLeft = false
         }
 
@@ -243,6 +363,7 @@ export default function App() {
         document.addEventListener('keydown', startMoving_2)
 
     }
+
     function stop() {
         document.addEventListener('keyup', stopMoving_1)
         document.addEventListener('keyup', stopMoving_2)
@@ -250,32 +371,45 @@ export default function App() {
 
 
     return (
-        <div className={'fieldWW'}>
-            <div id={'top'}>
-
+        <div>
+            <div style={{display:'flex', flexDirection:'row' ,margin:'40px', justifyContent:'space-around'}}>
+            <div style={{color:'blue', fontWeight:'bold', fontSize:"40px"}}> BLUE POINTS : {countBlue} </div>
+                <div style={{color:'yellow', fontWeight:'bold', fontSize:"40px"}}>YELLOW POINTS : {countYellow}</div>
             </div>
-            <div id={'left'}>
+                <div className={'fieldWW'}>
+                <div id={'top'}>
 
-            </div>
-            <div id={'right'}>
-
-            </div>
-            <div className={'field'}  onKeyDown={move()} onKeyUp={stop()}>
-                <div className={'cars'}>
-                    <i id={'driftingCar_1'} style={{color: '#3A75C4 ', fontSize: '60px'}} className="fas fa-fighter-jet"> </i>
-                    <i id={'driftingCar_2'} style={{color: '#FDE910 ', fontSize: '60px'}} className="fas fa-fighter-jet"> </i>
                 </div>
-                <div className={'walls'}>
-                    <div id={'first_turn'}> </div>
-                    <div id={'second_turn'}> </div>
-                    <div id={'third_turn'}> </div>
-                    <div id={'fourth_turn'}> </div>
+                <div id={'left'}>
+
+                </div>
+                <div id={'right'}>
+
+                </div>
+                <div className={'field'} onKeyDown={move()} onKeyUp={stop()}>
+                    <div className={'cars'}>
+                        <i id={'driftingCar_1'} style={{color: '#3A75C4 ', fontSize: '60px'}}
+                           className="fas fa-fighter-jet"> </i>
+                        <i id={'driftingCar_2'} style={{color: '#FDE910 ', fontSize: '60px'}}
+                           className="fas fa-fighter-jet"> </i>
+                    </div>
+                    <div className={'walls'}>
+                        <div id={'first_turn'}>
+                        </div>
+                        <div id={'second_turn'}>
+                        </div>
+                        <div id={'third_turn'}>
+                        </div>
+                        <div id={'fourth_turn'}>
+                        </div>
+                        <div id={'finish'}>
+                        </div>
+                    </div>
+                </div>
+                <div id={'bottom'}>
+
                 </div>
             </div>
-            <div id={'bottom'}>
-
-            </div>
-
         </div>
     );
 }
